a6c2e80fe02f647793e7632d8fd7948c6612e4cb
Victor Araujo <victtordan10@gmail.com>
2024-11-28 09:15:11 -0300

    correções video component

--- src/app/components/video/video.component.ts
+++ src/app/components/video/video.component.ts
@@ -1,27 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { Video } from '../../../video';
-import { VideoItemComponent } from '../video-item/video-item.component';
 import { VideoServiceService } from '../../services/video-service.service';
 import { CommonModule } from '@angular/common';
+import { Video } from '../../../video';
 
 @Component({
   selector: 'app-video',
   standalone: true,
-  imports: [VideoItemComponent, CommonModule],
+  imports: [CommonModule],
   templateUrl: './video.component.html',
   styleUrl: './video.component.scss'
 })
+
 export class VideoComponent implements OnInit {
 
-  videos = vid
+  videos: Video[] = [];
 
-  constructor(private videoService: VideoServiceService){}
+  constructor(private videoService: VideoServiceService) { }
 
   ngOnInit(): void {
     this.videoService.getVideos().subscribe((dado) => {
       this.videos = dado;
       console.log(dado);
     })
+  }
+
+  watchVideo(video: Video) {
+    this.videoService.getVideoWatch(video).subscribe(() =>
+      (this.videos = this.videos.filter((v) => v.id != video.id)));
   }
 
 }
