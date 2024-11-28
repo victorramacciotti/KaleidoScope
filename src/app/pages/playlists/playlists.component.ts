dc89a30e99811702b5727e415f0cebcce322c408
Victor Araujo <victtordan10@gmail.com>
2024-11-28 08:34:48 -0300

    criação do componente de playlists

---
+++ src/app/pages/playlists/playlists.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-playlists',
+  standalone: true,
+  imports: [],
+  templateUrl: './playlists.component.html',
+  styleUrl: './playlists.component.scss'
+})
+export class PlaylistsComponent {
+
+}
