68760491a7cfba48ca07b2a7c8a8d217e098d0d7
Victor Araujo <victtordan10@gmail.com>
2024-11-29 18:45:32 -0300

    angular material

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HomePageComponent } from "../pages/home-page/home-page.component";
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [],
+  imports: [HomePageComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { VideoItemComponent } from "../../components/video-item/video-item.component";
+import { Video } from '../../../video';
+import { VideoServiceService } from '../../services/video-service.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [VideoItemComponent, CommonModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
 export class HomePageComponent {
+  videos: Video[] = [];
 
+  constructor(private videoService: VideoServiceService) { }
+
+  ngOnInit(): void {
+    this.videoService.getVideos().subscribe((dado) => {
+      this.videos = dado;
+      console.log(dado);
+    })
+  }
+
+  watchVideo(video: Video) {
+    this.videoService.getVideoWatch(video).subscribe(() =>
+      (this.videos = this.videos.filter((v) => v.id != video.id)));
+  }
 }
