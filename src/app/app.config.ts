ca50dfc31439db616ad342e73432beb1d6e76e4b
Victor Araujo <victtordan10@gmail.com>
2024-11-30 22:41:42 -0300

    adição login com OAuth0

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,25 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideAuth0 } from '@auth0/auth0-angular';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
+              provideRouter(routes), 
+              provideHttpClient(),
+              provideAuth0({
+
+                domain: 'dev-7fvb7yro48pcduqm.us.auth0.com',
+               
+                clientId: 'mHwMrnhPwEHbleoJvE107fdUbi1BsD2f',
+               
+                authorizationParams: {
+               
+                redirect_uri: window.location.origin,
+               
+                },
+               
+                }),
+            ]
 };
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { SearchBarComponent } from '../components/search-bar/search-bar.component';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-navbar',
@@ -10,6 +11,13 @@ import { SearchBarComponent } from '../components/search-bar/search-bar.componen
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  constructor(public auth: AuthService) {}
+
+  login() {
+
+    this.auth.loginWithRedirect();
+   
+    }
 
 
 }
