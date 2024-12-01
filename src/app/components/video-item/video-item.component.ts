79847b1c48ebb05c0cb10d42b2c68e340b7ae3b2
Victor Araujo <victtordan10@gmail.com>
2024-11-30 22:05:54 -0300

    edições de aparência e funcionalidades

--- src/app/components/video-item/video-item.component.ts
+++ src/app/components/video-item/video-item.component.ts
@@ -2,20 +2,29 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Video } from '../../../video';
 import { MatCardModule } from '@angular/material/card';
+import { VideoServiceService } from '../../services/video-service.service';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-video-item',
   standalone: true,
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, FormsModule, RouterLink],
   templateUrl: './video-item.component.html',
   styleUrl: './video-item.component.scss'
 })
 export class VideoItemComponent {
   @Input() video!: Video;
   @Output() onWatchVideo = new EventEmitter<Video>();
+  videoIdText: string = '';
 
-  onWatch(video: Video) {
-    this.onWatchVideo.emit(video);
+
+  constructor(private videoService: VideoServiceService) {}
+ 
+
+  goWatch(videoIdText:string){
+    console.log('Video ID clicado:', videoIdText); 
+    this.videoService.setVideoId(videoIdText);
   }
 
 }
