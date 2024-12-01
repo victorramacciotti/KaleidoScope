0aa3a76c84c9358b88ebcc6f73c226a04a34ff54
Victor Araujo <victtordan10@gmail.com>
2024-11-30 22:04:01 -0300

    adição route para pagina de pesquisa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { PlaylistsComponent } from './pages/playlists/playlists.component';
+import { PlayComponent } from './pages/play/play.component';
+import { SearchComponent } from './pages/search/search.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomePageComponent },
+    { path: 'homePage', component: HomePageComponent },
+    { path: 'myPlaylists', component: PlaylistsComponent},
+    { path: 'watch', component: PlayComponent},
+    { path: 'search', component: SearchComponent},
+    { path: '**', redirectTo: '/homePage' },
+];
