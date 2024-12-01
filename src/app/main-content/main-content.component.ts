2b331ddf323d30472c1ce6c3c3f8c1435bc0ebf3
Victor Araujo <victtordan10@gmail.com>
2024-11-30 22:07:26 -0300

    router-outlet import

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { HomePageComponent } from "../pages/home-page/home-page.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [HomePageComponent],
+  imports: [RouterOutlet],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
