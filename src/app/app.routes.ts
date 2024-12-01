81bdf6cce163a29945806be505ff5fa62599f4e7
Victor Araujo <victtordan10@gmail.com>
2024-11-30 23:10:17 -0300

    adequações

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { PlaylistsComponent } from './pages/playlists/playlists.component';
 import { PlayComponent } from './pages/play/play.component';
 import { SearchComponent } from './pages/search/search.component';
+import { authGuardFn } from '@auth0/auth0-angular';
 
 export const routes: Routes = [
     { path: '', component: HomePageComponent },
     { path: 'homePage', component: HomePageComponent },
-    { path: 'myPlaylists', component: PlaylistsComponent},
+    { path: 'myPlaylists', component: PlaylistsComponent, canActivate: [authGuardFn]},
     { path: 'watch', component: PlayComponent},
     { path: 'search', component: SearchComponent},
     { path: '**', redirectTo: '/homePage' },
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,23 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { SearchBarComponent } from '../components/search-bar/search-bar.component';
-import { AuthService } from '@auth0/auth0-angular';
+import { AuthService, User } from '@auth0/auth0-angular';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, SearchBarComponent],
+  imports: [RouterLink, SearchBarComponent, AsyncPipe],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   constructor(public auth: AuthService) {}
+  profile?: User | undefined | null;
 
-  login() {
+  ngOnInit(): void {
+    this.auth.user$.subscribe((profile) => {
+      this.profile = profile;
+    });
+}
+
+  /*login() {
 
     this.auth.loginWithRedirect();
    
     }
+  logout() {
+    this.auth.logout({
+    
+    logoutParams: {returnTo: this.document.location.origin,},
+    
+    });
+    
+    }*/
 
 
 }
